f6bf038473c8fcf15d0329df0c60ae2318fe1715
spasiu <stasi@smooch.io>
2018-01-17 15:30:51 -0500

    Initial Commit

---
+++ custom-ui-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  custom-ui-ios
+//
+//  Created by Anastasi Bakolias on 2018-01-17.
+//  Copyright © 2018 Anastasi Bakolias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
